a82eb2599e09b7372dc53c526b1b7718ea554d79
JavierOramas <javiale2000@gmail.com>
2023-05-02 09:42:04 -0400

    urban-rural household distribution

---
+++ simulation/Household.py
@@ -0,0 +1,10 @@
+from data_distribution import prob_density
+
+
+class Household:
+    def __init__(self, province, neighborhood):
+        self.number_of_persons = np.random.choice(9, 1, p=prob_density)[0]+1
+
+        self.persons = []
+        self.province = province
+        self.neighborhood = neighborhood

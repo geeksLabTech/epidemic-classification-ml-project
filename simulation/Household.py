c82add4ac62fd314c115219fe2afc27457e07679
JavierOramas <javiale2000@gmail.com>
2023-05-10 03:22:13 -0400

    more code commenting

--- simulation/Household.py
+++ simulation/Household.py
@@ -3,9 +3,36 @@ import numpy as np
 
 
 class Household:
-    def __init__(self, province, neighborhood):
+    """Class that represents a Household in the world
+
+    Attributes:
+    -----------
+    number_of_persons (int):
+        an int between 1 and 9, randomly generated on instanciation, represents the number of
+        people in the house
+    persons (list[Person]):
+        a list containing the instances of Person for the house inhabitants
+    province (str):
+        a string with the province name
+    neighborhood (int):
+        an int with the neighborhood number, to identify locality
+    """
+
+    def __init__(self, province: str, neighborhood: int):
+        """
+
+        Args:
+            province (str): province name
+            neighborhood (int): neighborhood ID
+        """
+
+        # according to household size probability in data_distribution
+        # a number of inhabitants is generated
+        # min val: 1, max val: 9
         self.number_of_persons = np.random.choice(9, 1, p=prob_density)[0]+1
 
+        # initially persons list is empty, on World creation, the inhabitants will be added
         self.persons = []
+
         self.province = province
         self.neighborhood = neighborhood

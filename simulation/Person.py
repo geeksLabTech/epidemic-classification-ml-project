76ec89589c49ec2cec7a64c688356acb55d8122f
Joramas <javiale2000@gmail.com>
2023-05-10 03:22:13 -0400

    more code commenting

--- simulation/Person.py
+++ simulation/Person.py
@@ -3,42 +3,89 @@ from data_distribution import *
 
 
 class Person:
-    def __init__(self):
+    """Class representing a Person, the core of the simulation.
 
+        Attributes:
+        -----------
+        age_group (int):
+            one of the 14 age groups (< 5, 5-9, 10-14, 15-19, 20-24, 25-29, 30-34, 35-39,40-44, 45-49, 50-54, 55-59,60-64,65>)
+        age (int):
+            integer representing actual age
+        sex (bool):
+            bool representing sex (False: male, True: female)
+        work (bool):
+            bool representing if the person works or not 
+        study (bool):
+            bool representing if a person studies or not
+        study_details (str):
+            string denoting school_type, it is assigned acording to age
+        economic_activity (int):
+            integer representing one of the 16 economic activities in distribution
+    """
+
+    def __init__(self):
+        """Person generation takes all demographic data to fill needed fields
+        """
+
+        # age_group is assigned according to the distibution
         self.age_group = np.random.choice(
             14, 1, p=distribution_by_age_groups)[0]
 
+        # once the age group is selected, an uniform distribution is assumed
+        # for all ages within the age range on the age group
+        # age is then selected
         ages = [i for i in range(
             age_groups[self.age_group][0], age_groups[self.age_group][1])]
-
         self.age = np.random.choice(ages, 1)[0]
-        self.sex = np.random.choice(2, 1, p=[
-                                    distribution_of_woman_or_man[0], distribution_of_woman_or_man[1]+distribution_of_woman_or_man[2]])[0]
-        self.work = self.study = False
+        del ages
 
+        # sex is also selected using the probabilities in data_distribution
+        self.sex = bool(np.random.choice(2, 1, p=[
+            distribution_of_woman_or_man[0], distribution_of_woman_or_man[1]+distribution_of_woman_or_man[2]])[0])
+
+        # initially work and study are false
+        # for people with 15 or less years
+        # it is mandatory to go to corresponding school
+        self.work = self.study = False
         if self.age > 6 and self.age < 13:
-            self.study = 'primary'
-        # Enrollment and employment
+            self.study_details = 'primary'
+            self.study = True
         elif self.age <= 15:
-            self.study = 'secondary`'
+            self.study_details = 'secondary'
+            self.study = True
+
         # pre-universitary is assigned according to the distribution
         elif self.age > 15 and self.age < 18:
             if random.random() < enrollment_distribution[2]/total_population:
-                self.study = 'pre_univ'
-        # for university, in case of a person being over 30 years the probability is gradually reduced
+                self.study_details = 'pre_univ'
+                self.study = True
+
+        # for university, in case of a person being over 30 years
+        # the probability of studying is gradually reduced
         elif self.age > 18:
             population_coef = total_population
             if self.age > 30:
                 population_coef *= self.age/10
             if random.random() < enrollment_distribution[3]/population_coef:
-                self.study = 'university'
+                self.study_details = 'university'
+                self.study = True
 
+        # if the person if older than 15, according to its sex
+        # and the distribution, its determined wether or not it
+        # is an active worker
         if self.age > 15 and self.age < 75:
-            self.work = True
-
-            if self.sex == 0:
-                act = man_distribution_by_activity_economic
+            if self.sex:
+                active = np.random.choice(2, 1, p=active_woman_in_working_age)
             else:
-                act = women_distribution_by_activity_economic
+                active = np.random.choice(2, 1, p=active_man_in_working_age)
 
-            self.economic_activity = np.random.choice(16, 1, act)
+            if active:
+                self.work = True
+
+                # select corresponding distribution given sex
+                if not self.sex:
+                    act = man_distribution_by_activity_economic
+                else:
+                    act = women_distribution_by_activity_economic
+                # economic activity is selected
+                self.economic_activity = np.random.choice(16, 1, act)
--- simulation/School.py
+++ simulation/School.py
@@ -1,6 +1,22 @@
 class School:
+    """Class that represents Schools
 
-    def __init__(self, province, school_type):
+    Attributes:
+    -----------
+    province (str):
+        a string with the name of the province, for more detailed matrices
+    school_tipe (str):
+        a formatted string with one of the posible values ('primary','secondary','pre_univ','university')
+    students (list[Person]):
+        a list of instances of Person, with all the students in the school
+    """
+
+    def __init__(self, province: str, school_type: str):
+        """
+        Args:
+            province (str): the province name
+            school_type (str): the school type
+        """
         self.province = province
         self.school_type = school_type
         self.students = []

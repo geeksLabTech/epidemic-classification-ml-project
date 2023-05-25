11536a97dab5d54e9ee6579a3a49ea1131b3f079
Lia <liazerqueraf@gmail.com>
2023-05-24 23:54:07 -0400

    school matrix , work matrix and house matrix

--- simulation/simulationv2.py
+++ simulation/simulationv2.py
@@ -9,33 +9,24 @@ import pickle
 from pathlib import Path
 from World import World
 
-def run_step_of_simulation(graph : Graph,db: SyncEngine ,matrix , w : World):
+def run_step_of_simulation(graph : Graph,db: SyncEngine, matrix,Schoolmatrix, Workmatrix,Housematrix , w : World):
         
-    
-    # myclient = MongoClient("mongodb://localhost:27017/")
-    # db = myclient["contact_simulation"]
-    # population_Collection = db["Person"
     population = db.find(Person)
     for person in population:
+        update_visitors(person,graph.id_nodes[person.current_place])
         move = build_move_distribution(person.last_place,person.current_place,graph,person.study,person.work)
-        # print(type(move), ' es un node')
-        # print(move in graph.nodes)
-        # print(type(person))
-        # print(type(move), 'que tu ere')
-
         move.visitors.append(person)
         # print(type(move.visitors[-1]), 'que tu ere')
     print('termine esto')
-    return get_contact_matrix(graph,population,matrix,w)
+    return get_contact_matrix(graph,population,matrix,Schoolmatrix,Workmatrix,Housematrix,w)
 
 
 def build_move_distribution(last_position:str,actual_position:str,graph:Graph,isSchool,isWork) -> Node:
     node_actual = graph.id_nodes[actual_position]
+   
     possible_next_moves = graph.nodes[node_actual]
     move_list = [] 
     prob_list =[0.0]*len(possible_next_moves)
-    # print(prob_list,'moves')
-    # pro_move_random = (len(possible_next_moves) - 2)/0.3
     for i in range (len(possible_next_moves)):
         move_list.append(possible_next_moves[i])
         if possible_next_moves[i].type == 'H':
@@ -43,7 +34,8 @@ def build_move_distribution(last_position:str,actual_position:str,graph:Graph,is
                 prob_list[i] = 0.4
             elif not graph.id_nodes[last_position].type == 'H' :
                 if graph.id_nodes[last_position].type == 'W' or graph.id_nodes[last_position].type == 'S':
-                    prob_list[i] = 0.4
+                    prob_list[i] = 0.3
+                    
                 else :
                     prob_list[i] = 0.2
             else:
@@ -75,72 +67,59 @@ def build_move_distribution(last_position:str,actual_position:str,graph:Graph,is
             prob_list[i] = 0.1
         else:
             prob_list[i] = 0.1
-    # print(prob_list, 'kdkd')
+
     prob_list = np.array(prob_list) / np.linalg.norm(prob_list, ord=1)
-    # print(move_list)
-    # print(prob_list)
     position = np.random.choice(a =len(move_list), p = prob_list,size=1)[0]
-    # print(position, ' a ver')
-    # print(position)
-    # print(move_list, 'mira')
-    # print(move_list[position], ' que bola')
     if not move_list[position].type == 'N':
         return move_list[position]
     else:
-        # print(graph.nodes[move_list[position]], ' a ver si es normal')
         if len(graph.nodes[move_list[position]] )== 0:
             return graph.id_nodes[actual_position]
         else:
             house_random = random.randint(0,len(graph.nodes[move_list[position]])-1)
-            # print(house_random, ' esto es un numero ??')
             return graph.nodes[move_list[position]][house_random]
 
-                
-def get_contact_matrix(graph: Graph,population,matrix,w : World):
-    # print('total nodes, ', len(graph.nodes.keys()))
+def update_visitors(person:Person,current_place:Node):
+    for i in range (len(current_place.visitors)-1):
+        if current_place.visitors[i] == person:
+            current_place.visitors.pop(i)
+        
+
+
+
+
+def get_contact_matrix(graph: Graph,population,matrix,Schoolmatrix,Workmatrix,Housematrix,w : World):
     z =0
     for node in graph.nodes.keys():
-        # print(type(node))
-        # print(z)
-        # print('cant visitors', len(node.visitors), 'tipo', node.type)
-
-        # if z == len(graph.nodes) - 1 :
-        #     print('estoy en ultimo nodo')
-        # print(node.type, ' pa ver')
         if node.visitors == []:
             continue
+        print(node.type , 'mm', len(node.visitors), ' mmm')
         if not node.type == "H":
             
             number_contact = len(node.visitors)/10
-            
-
             get_normal_distribution = np.arange(0,len(node.visitors))
-            # prob = np.array(get_normal_distribution) / np.linalg.norm(get_normal_distribution, ord=1)
             contacts = np.random.choice(a = len(node.visitors), size = int(number_contact))
             for i in range(len(contacts)):
-            #    if z == 292450:
-
-            #     print('mmmm', i, len(contacts))
-               for j in range(i+1,len(contacts)): 
-                    matrix[w.get_age_group(node.visitors[contacts[i]].age),w.get_age_group(node.visitors[contacts[j]].age)] += 1
-                    matrix[w.get_age_group(node.visitors[contacts[j]].age),w.get_age_group(node.visitors[contacts[i]].age)] += 1
-                 
+               for j in range(i+1,len(contacts)):
+                    if node.type == 'S':
+                        Schoolmatrix[w.get_age_group(node.visitors[contacts[i]].age),w.get_age_group(node.visitors[contacts[j]].age)] += 1
+                        Schoolmatrix[w.get_age_group(node.visitors[contacts[j]].age),w.get_age_group(node.visitors[contacts[i]].age)] += 1
+                    elif node.type == 'W':
+                        Workmatrix[w.get_age_group(node.visitors[contacts[i]].age),w.get_age_group(node.visitors[contacts[j]].age)] += 1
+                        Workmatrix[w.get_age_group(node.visitors[contacts[j]].age),w.get_age_group(node.visitors[contacts[i]].age)] += 1
+                    matrix[w.get_age_group(node.visitors[i].age),w.get_age_group(node.visitors[j].age)] += 1
+                    matrix[w.get_age_group(node.visitors[j].age),w.get_age_group(node.visitors[i].age)] += 1
         else:
-            # if z == 292449:
-            #     print('murio aki')
-            # print('comienza else')
-            # print(len(node.visitors))
             for i in range(len(node.visitors)):
                 for j in range(i+1,len(node.visitors)):
-                   
+                    Housematrix[w.get_age_group(node.visitors[i].age),w.get_age_group(node.visitors[j].age)] += 1
+                    Housematrix[w.get_age_group(node.visitors[j].age),w.get_age_group(node.visitors[i].age)] += 1
                     matrix[w.get_age_group(node.visitors[i].age),w.get_age_group(node.visitors[j].age)] += 1
                     matrix[w.get_age_group(node.visitors[j].age),w.get_age_group(node.visitors[i].age)] += 1
         z+=1
     print('sali del for')
-    # print(matrix)
-
-                
-    return matrix
+    # print(matrix)         
+    return matrix, Schoolmatrix,Workmatrix,Housematrix
     
 
 def save_graph_to_file(graph: Graph):
@@ -164,15 +143,22 @@ def run_simulation(world : World,use_cache=True):
         graph = build_graph()
         save_graph_to_file(graph)
         print('done graph')
-    # print(len(graph.nodes))
-    # print(len(graph.id_nodes))
+
     
         
-    M = matrix = np.zeros((14,14))
-    # for i in range(3):
-
-    M = run_step_of_simulation(graph,db,M ,world)
-    print(M)
+    M =  np.zeros((14,14))
+    SM = np.zeros((14,14))
+    WM = np.zeros((14,14))
+    HM = np.zeros((14,14))
+    for i in range(2):
+        print(i)
+        M,SM,WM,HM = run_step_of_simulation(graph,db,M,SM,WM,HM ,world)
+        print('School contacts Matrix')
+        print(SM)
+        print('Work contacts Matrix')
+        print(WM)
+        print('House contacts Matrix')
+        print(HM)
 
 
         

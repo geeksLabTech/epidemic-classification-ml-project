769e7c82a0417ef82e860c8613ecdbdcc074327c
Lia <liazerqueraf@gmail.com>
2023-05-31 00:02:35 -0400

    add dataset and split dataset

--- autogoal.py
+++ autogoal.py
@@ -1,11 +1,23 @@
 from autogoal.ml import AutoML
 from autogoal.kb import Vector, Matrix, Supervised
 from autogoal.search import RichLogger
-from data_loader import DataLoader
+# from data_loader import DataLoader
 import pandas as pd
 import numpy as np
+from models.contact_matrix import ContactMatrix
+from odmantic import SyncEngine
+from sklearn.model_selection import train_test_split
 
 
+db = SyncEngine(database='contact_simulation')
+X = []
+Y = []
+matrix = db.find(ContactMatrix)
+for i in matrix:
+    X.append(i.vector)
+    Y.append(i.data)
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size= None,train_size=None, random_state=None, shuffle=True, stratify=None)
 automl = AutoML(
     input=(Vector, Supervised[Vector]),
     output=Matrix, )

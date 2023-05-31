c992789062c4aeb0cef44805af5816949c8b4bf3
Lia <liazerqueraf@gmail.com>
2023-05-31 00:03:08 -0400

    add vectorize to the model

--- models/contact_matrix.py
+++ models/contact_matrix.py
@@ -4,7 +4,7 @@ from odmantic import Model
 class ContactMatrix(Model):
     category: str 
     # Integer that represent in which iteration was the matrix builded
-    iteration: int
+    vector : list[float]
     data: list[list[float]]
     # This is for allow storing matrices of different simulation styles
     simulation_type: str
